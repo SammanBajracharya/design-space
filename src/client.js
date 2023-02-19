061132e3370b0e32c41e53946d1cf00468951a4e
SammanBajracharya <107597132+SammanBajracharya@users.noreply.github.com>
2023-02-19 19:48:33 +0545

    client

--- src/client.js
+++ src/client.js
@@ -1,5 +1,7 @@
 import { createClient } from 'contentful';
 
+console.log(process.env);
+
 export const client = createClient({
   space: process.env.REACT_APP_SPACE_ID,
   accessToken: process.env.REACT_APP_ACCESS_TOKEN,
