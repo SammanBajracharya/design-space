3517a8cbfb7d83c16ef4e56677b039a4ca334dbb
SammanBajracharya <107597132+SammanBajracharya@users.noreply.github.com>
2023-02-19 19:52:51 +0545

    remove console.logprocessenv

--- src/client.js
+++ src/client.js
@@ -1,7 +1,5 @@
 import { createClient } from 'contentful';
 
-console.log(process.env);
-
 export const client = createClient({
   space: process.env.REACT_APP_SPACE_ID,
   accessToken: process.env.REACT_APP_ACCESS_TOKEN,
