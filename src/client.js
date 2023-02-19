7e0220cf9e72a6221c80ff4aac7ee114f67664cb
SammanBajracharya <107597132+SammanBajracharya@users.noreply.github.com>
2023-02-19 18:51:34 +0545

    'apis'

--- src/client.js
+++ src/client.js
@@ -1,7 +1,7 @@
 import { createClient } from 'contentful';
-import { SPACE_ID, ACCESS_TOKEN } from './API';
+
 
 export const client = createClient({
-  space: SPACE_ID,
-  accessToken: ACCESS_TOKEN,
+  space: process.env.SPACE_ID,
+  accessToken: process.env.ACCESS_TOKEN,
 });
